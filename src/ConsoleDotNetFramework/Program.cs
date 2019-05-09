d65a7bf6f15d11cfe50de43dcbaba1ed9fc47847
Fabiano Conrado <fabianoconrado@gmail.com>
2019-05-08 21:14:21 -0300

    Incluindo arquivos...

---
+++ src/ConsoleDotNetFramework/Program.cs
@@ -0,0 +1,23 @@
+﻿using ConsoleDotNetFramework.Model;
+using NHibernate;
+using NHibernate.Cfg;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleDotNetFramework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var session = NHibernateHelper.GetCurrentSession();
+
+            var pessoa1 = new Pessoa("João Paulo");
+
+            session.Save(pessoa1);
+        }
+    }
+}
